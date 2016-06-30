3cd0d7f13763b4638b185f2930ce06805a79cbae
Constantin Pape <c.pape@gmx.net>
2016-06-30 09:27:08 +0200

    Adding marray and pybind submodules

---
+++ include/escargot/marray/marray.hxx
@@ -0,0 +1,11 @@
+// wrapping andres::marray to have it in the escargot namespace
+
+#pragma once
+
+#include <andres/marray.hxx>
+
+namespace escargot{
+namespace marray{
+    using namespace andres;
+}
+}
