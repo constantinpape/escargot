97c04d08f0aa9f94d2f5a3f82e22cf220a254e20
Constantin Pape <c.pape@gmx.net>
2016-06-30 09:19:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Efficient, SCalabale and Resourceful Graph algOriThms
+
+Implementing graph - based algorithms for computer vision.
+
+## Features
+
+* Pixel affinity watershed after ... 
+* ... 
---
+++ include/escargot/tools/tools.hxx
@@ -0,0 +1,14 @@
+#include<utility>
+
+namespace graphWatershed {
+    
+    typedef std::pair<size_t,size_t> coordinate_type;
+
+    // sorting function (we sort in DECREASING order !)
+    template<class value_type>
+    bool sortByValDecreasing(std::pair<coordinate_type,value_type> first, std::pair<coordinate_type,value_type> second )
+    {
+        return (first >= second) ? false : true;
+    }
+
+}
